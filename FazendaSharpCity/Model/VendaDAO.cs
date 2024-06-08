b00bbe72428c2d5f43f77f70690f1154c4b65669
Pedro Paulo Barbosa <147881032+PedroPTGB@users.noreply.github.com>
2024-06-08 00:14:15 -0300

    Create VendaDAO.cs

---
+++ FazendaSharpCity/Model/VendaDAO.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace FazendaSharpCity.Model
+{
+    internal class VendaDAO : IDAO
+    {
+        public NpgsqlConnection Connection { get; set; } = null;
+
+        public VendaDAO(string host, string port, string database, string user, string pass)
+        {
+            Connect(host, port, database, user, pass);
+        }
+
+
+        public System.Data.DataTable Search(VendaModel venda)
+        {
+            string query = "SELECT * FROM venda WHERE idvenda = @ID ORDER BY idvenda;";
+
+            NpgsqlCommand c2 = new NpgsqlCommand(query, Connection);
+
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(c2);
+
+            c2.Parameters.AddWithValue("ID", venda.idVenda);
+
+            System.Data.DataTable table = new System.Data.DataTable();
+            da.Fill(table);
+
+            return table;
+
+        }
+
+        public System.Data.DataTable List()
+        {
+            string query = "SELECT * FROM venda ORDER BY idvenda;";
+            NpgsqlCommand c2 = new NpgsqlCommand(query, Connection);
+
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(c2);
+
+            System.Data.DataTable table = new System.Data.DataTable();
+            da.Fill(table);
+            return table;
+        }
+
+        public void Insert(VendaModel venda)
+        {
+            string query = "INSERT INTO venda (precounitario, dtvenda, formapagamento, quantidade) VALUES (@PrecoU, @DtVenda, @FormPag, @QTD)";
+
+            NpgsqlCommand c2 = new NpgsqlCommand(query, Connection);
+
+            c2.Parameters.AddWithValue("PrecoU", venda.precoUnit);
+            c2.Parameters.AddWithValue("DtVenda", venda.dtVenda);
+            c2.Parameters.AddWithValue("FormPag", venda.formaPag);
+            c2.Parameters.AddWithValue("QTD", venda.qtd);
+
+            c2.ExecuteNonQuery();
+        }
+
+        public void Update(VendaModel venda)
+        {
+            string query = "UPDATE venda SET " +
+                "precounitario = @PrecoU, " +
+                "dtvenda = @DtVenda, " +
+                "formapagamento = @FormPag, " +
+                "quantidade = @QTD " +
+                "WHERE idvenda = @ID;";
+
+            NpgsqlCommand c2 = new NpgsqlCommand(query, Connection);
+
+            c2.Parameters.AddWithValue("ID", venda.idVenda);
+            c2.Parameters.AddWithValue("PrecoU", venda.precoUnit);
+            c2.Parameters.AddWithValue("DtVenda", venda.dtVenda);
+            c2.Parameters.AddWithValue("FormPag", venda.formaPag);
+            c2.Parameters.AddWithValue("QTD", venda.qtd);
+
+            c2.ExecuteNonQuery();
+        }
+
+        public void Delete(VendaModel venda)
+        {
+            string query = "DELETE FROM venda WHERE idvenda = @ID";
+            NpgsqlCommand c2 = new NpgsqlCommand(query, Connection);
+
+            c2.Parameters.AddWithValue("ID", venda.idVenda);
+
+            c2.ExecuteNonQuery();
+        }
+
+        private void Connect(string host, string port, string database, string user, string pass)
+        {
+            if (Connection == null)
+            {
+                Connection = new NpgsqlConnection(
+                $"Server={host};" +
+                $"Port={port};" +
+                $"Database={database};" +
+                $"Uid={user};" +
+                $"Pwd={pass};");
+
+                Connection.Open();
+            }
+        }
+
+        private void Disconnect()
+        {
+            if (Connection != null)
+            {
+                Connection.Close();
+            }
+        }
+
+    }
+
+}
