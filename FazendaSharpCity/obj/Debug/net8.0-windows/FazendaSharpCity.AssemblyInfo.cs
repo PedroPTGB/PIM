1d95e0795de4e85beca50b4f11076f4e2a3c9883
Gabriel.L <gabrielestefanoleite01@gmail.com>
2024-06-08 00:53:48 -0300

    Add forms de vendas e interface grafica MAIN

--- FazendaSharpCity/obj/Debug/net8.0-windows/FazendaSharpCity.AssemblyInfo.cs
+++ FazendaSharpCity/obj/Debug/net8.0-windows/FazendaSharpCity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FazendaSharpCity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4971b20636b520c2df8a5411b989453d5ca9eafe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+071aad99abbd91c7aa3680af3683f19f7776ee2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FazendaSharpCity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FazendaSharpCity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
